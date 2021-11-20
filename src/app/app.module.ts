605c161014fdd98855a0bc0837ad16fa3d3560d3
William Menghi Hirano <whirano4@gmail.com>
2021-11-20 20:56:08 -0300

    Adding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './view/home/home.component';
+import { FaqComponent } from './view/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
